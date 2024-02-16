af0f2c3f715c5cbcb676eab85781df84a3571e07
Breno Cabral <brcabral@gmail.com>
2024-02-16 16:03:22 -0300

    2.7. Trabalhando com variáveis

---
+++ src/cap02-Fundamentos_Linguagem/Variavel1.java
@@ -0,0 +1,10 @@
+public class Variavel1 {
+    public static void main(String[] args) {
+        int quantidade; // Declarando variável do tipo inteiro
+        quantidade = 10; // atribuindo o valor 10
+
+        System.out.println(quantidade);
+        quantidade = 15;
+        System.out.println(quantidade);
+    }
+}
