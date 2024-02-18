80f935c205c19361cfca1a2272a2c6f460c8602e
Breno Cabral <brcabral@gmail.com>
2024-02-18 16:09:10 -0300

    2.26. Operadores de incremento e decremento

---
+++ src/cap02-Fundamentos_Linguagem/IncrementoDecremento.java
@@ -0,0 +1,14 @@
+public class IncrementoDecremento {
+    public static void main(String[] args) {
+        int idade = 10;
+        int novaIdade = idade++;
+
+        System.out.println("Idade: " + idade);
+        System.out.println("Nova idade: " + novaIdade);
+
+        System.out.println();
+        novaIdade = ++idade;
+        System.out.println("Idade: " + idade);
+        System.out.println("Nova idade: " + novaIdade);
+    }
+}
