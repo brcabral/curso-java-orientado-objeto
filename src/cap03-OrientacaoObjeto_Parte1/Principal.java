195831d14f8bf33c2aa0215af17f8f9a2160d512
Breno Cabral <brcabral@gmail.com>
2024-02-19 23:12:00 -0300

    3.5. Acessando atributos de objetos

--- src/cap03-OrientacaoObjeto_Parte1/Principal.java
+++ src/cap03-OrientacaoObjeto_Parte1/Principal.java
@@ -1,6 +1,32 @@
 public class Principal {
     public static void main(String[] args) {
-        Carro meuCarro = new Carro();
-        Carro seuCarro = new Carro();
+        // Declarado a variável meuCarro
+		Carro meuCarro;
+		// Instanciado a variável com um objeto carro 
+		meuCarro = new Carro();
+		
+		meuCarro.fabricante = "FIAT";
+		meuCarro.modelo = "Palio";
+		meuCarro.anoDeFabricacao = 2011;
+		meuCarro.cor = "Preto";
+		
+		Carro seuCarro = new Carro();
+		seuCarro.fabricante = "Honda";
+		seuCarro.modelo = "Civic";
+		seuCarro.anoDeFabricacao = 2009;
+		seuCarro.cor = "Preto";
+		
+		System.out.println("---------- Meu carro ----------");
+		System.out.println("Modelo: " + meuCarro.fabricante);
+		System.out.println("Modelo: " + meuCarro.modelo);
+		System.out.println("Ano: " + meuCarro.anoDeFabricacao);
+		System.out.println("Ano: " + meuCarro.cor);
+		
+		System.out.println();
+		System.out.println("---------- Seu carro ----------");
+		System.out.println("Modelo: " + seuCarro.fabricante);
+		System.out.println("Modelo: " + seuCarro.modelo);
+		System.out.println("Ano: " + seuCarro.anoDeFabricacao);
+		System.out.println("Ano: " + seuCarro.cor);
     }
 }
