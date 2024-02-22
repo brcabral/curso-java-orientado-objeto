266c9508ff28c525bc34557b3ab9bde2e8a6db94
Breno Cabral <brcabral@gmail.com>
2024-02-21 23:01:52 -0300

    4.5. Exercício: arrays

---
+++ src/cap04-Wrappers_Boxing_Arrays/Estoque.java
@@ -0,0 +1,12 @@
+public class Estoque {
+    Produto[] produtos;
+
+    void listarProdutos() {
+        System.out.println(">> Produtos em estoque <<");
+        System.out.println("-------------------------");
+
+        for (int i = 0; i < produtos.length; i++) {
+            produtos[i].descrever();
+        }
+    }
+}
