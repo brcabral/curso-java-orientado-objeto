28c3857db36b975a63d34116c73267df5c9217e2
Breno Cabral <brcabral@gmail.com>
2024-02-24 23:08:55 -0300

    5.4. Construtores

---
+++ src/cap05-OrientacaoObjeto_Parte2/5.4-construtor/Pessoa.java
@@ -0,0 +1,14 @@
+public class Pessoa {
+    String nome;
+    int idade;
+
+    Pessoa(String nome) {
+        this.nome = nome;
+        // Inicialização bem complicada de fazer...
+    }
+
+    Pessoa(String nome, int idade) {
+        this(nome); // deve ser a primeira instrução dentro do construtor
+        this.idade = idade;
+    }
+}
