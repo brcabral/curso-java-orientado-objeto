f9ab2e8a924b2f74cb73a802c7f9b668e0ef6a01
Breno Cabral <brcabral@gmail.com>
2024-02-24 22:45:11 -0300

    5.3. O objeto this

---
+++ src/cap05-OrientacaoObjeto_Parte2/5.3-this/ExemploThis.java
@@ -0,0 +1,11 @@
+public class ExemploThis {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+
+        carro.modelo = "Palio";
+        System.out.println("Modelo antigo: " + carro.modelo);
+
+        carro.alterarModelo("Civic");
+        System.out.println("Novo modelo: " + carro.modelo);
+    }
+}
