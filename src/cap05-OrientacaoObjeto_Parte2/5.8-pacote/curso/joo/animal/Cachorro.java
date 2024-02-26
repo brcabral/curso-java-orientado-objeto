fa2dbada6a27bb5875f0a3310f86a50a2b4d3572
Breno Cabral <brcabral@gmail.com>
2024-02-25 23:33:04 -0300

    5.8. Organizando os projetos em pacotes

---
+++ src/cap05-OrientacaoObjeto_Parte2/5.8-pacote/curso/joo/animal/Cachorro.java
@@ -0,0 +1,13 @@
+package curso.joo.animal;
+
+public class Cachorro {
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ src/cap05-OrientacaoObjeto_Parte2/5.8-pacote/curso/joo/treinador/TreinadorCachorro.java
@@ -0,0 +1,10 @@
+package curso.joo.treinador;
+
+import curso.joo.animal.Cachorro;
+
+public class TreinadorCachorro {
+    public static void main(String[] args) {
+        Cachorro cachorro = new Cachorro();
+        cachorro.setNome("Bob");
+    }
+}
