1cb6fc4a3f59e1aa80ba4f85ce51129650531a62
Breno Cabral <brcabral@gmail.com>
2024-02-27 16:51:50 -0300

    5.20. Polimorfismo, casting de objetos e instanceof

---
+++ src/cap05-OrientacaoObjeto_Parte2/5.20-polimorfismo/curso/joo/polimorfismo/ContaPoupanca.java
@@ -0,0 +1,9 @@
+package curso.joo.polimorfismo;
+
+public class ContaPoupanca extends Conta {
+    private double rendimentos = 30;
+
+    public double getRendimentos() {
+        return rendimentos;
+    }
+}
