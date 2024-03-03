435eb3416542eac467723cccea706f4b7a8176f2
Breno Cabral <brcabral@gmail.com>
2024-03-03 16:38:16 -0300

    6.13. Desafio: collections

---
+++ src/cap06-TopicosAvancados/curso/joo/desafioCollections/Cargo.java
@@ -0,0 +1,24 @@
+package curso.joo.desafioCollections;
+
+import java.math.BigDecimal;
+
+public class Cargo {
+    private String descricao;
+    private BigDecimal salario;
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getSalario() {
+        return salario;
+    }
+
+    public void setSalario(BigDecimal salario) {
+        this.salario = salario;
+    }
+}
---
+++ src/cap06-TopicosAvancados/curso/joo/desafioCollections/Politico.java
@@ -0,0 +1,22 @@
+package curso.joo.desafioCollections;
+
+public class Politico {
+    private String nome;
+    private Cargo cargo;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Cargo getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(Cargo cargo) {
+        this.cargo = cargo;
+    }
+}
