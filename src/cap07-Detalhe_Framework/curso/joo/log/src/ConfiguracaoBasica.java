36dca14ac24ab6c12ac12ab213d8b63ab578af3e
Breno Cabral <brcabral@gmail.com>
2024-03-18 23:36:54 -0300

    7.12. Logging com log4j

---
+++ src/cap07-Detalhe_Framework/curso/joo/log/src/ConfiguracaoBasica.java
@@ -0,0 +1,14 @@
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
+public class ConfiguracaoBasica {
+	private static Logger logger = Logger.getLogger(ConfiguracaoBasica.class);
+
+	public static void main(String[] args) {
+		BasicConfigurator.configure();
+
+		logger.info("Começando a aplicação");
+		// código da aplicação
+		logger.debug("Finalizando a aplicação");
+	}
+}
---
+++ src/cap07-Detalhe_Framework/curso/joo/log/src/modelo/Cliente.java
@@ -0,0 +1,23 @@
+package modelo;
+
+public class Cliente {
+	private String nome;
+
+	public Cliente(String nome) {
+		super();
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [nome: " + nome + "]";
+	}
+}
