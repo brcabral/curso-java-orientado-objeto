cdc6837eb4401dd1872b3e37831f7e78bc33a6ec
Breno Cabral <brcabral@gmail.com>
2024-03-18 21:59:08 -0300

    7.10. Introdução a expressão regulares

---
+++ src/cap07-Detalhe_Framework/curso/joo/regex/ReplaceString.java
@@ -0,0 +1,9 @@
+package curso.joo.regex;
+
+public class ReplaceString {
+    public static void main(String[] args) {
+        String texto = "O valor do produto é R$ @xxx@.";
+        String novoTexto = texto.replaceAll("@x+@", "2,50");
+        System.out.println(novoTexto);
+    }
+}
