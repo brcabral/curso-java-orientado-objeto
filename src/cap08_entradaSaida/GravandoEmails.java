e3d71a0e571cb3bc31c90d906b643cb4f0b962d2
Breno Cabral <brcabral@gmail.com>
2024-03-24 18:38:57 -0300

    8.1. Gravando arquivo

---
+++ src/cap08_entradaSaida/GravandoEmails.java
@@ -0,0 +1,30 @@
+package cap08_entradaSaida;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintStream;
+
+public class GravandoEmails {
+    public static void main(String[] args) {
+        String[] emails = { "joao2@localhost.com", "maria2@localhost.com", "ricardo2@localhost.com" };
+
+        try (BufferedWriter writer = new BufferedWriter(
+                new FileWriter("./cap08_entradaSaida/files/emails.txt", true))) {
+            for (String email : emails) {
+                writer.write(email);
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            System.err.println("Não foi possível gravar o arquivo." + e.getMessage());
+        }
+
+        try (PrintStream writer = new PrintStream("./cap08_entradaSaida/files/emails2.txt")) {
+            for (String email : emails) {
+                writer.println(email);
+            }
+        } catch (IOException e) {
+            System.err.println("Não foi possível gravar o arquivo." + e.getMessage());
+        }
+    }
+}
