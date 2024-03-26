e026708c97b75ebf5081ab3fb6e0844fff171d37
Breno Cabral <brcabral@gmail.com>
2024-03-25 22:20:48 -0300

    9.3. Criando uma aplicação de chat

---
+++ src/cap09_serializacao/chat/chat-cliente/src/com/algaworks/chat/cliente/thread/RecebeMensagemServidor.java
@@ -0,0 +1,32 @@
+package com.algaworks.chat.cliente.thread;
+
+import java.io.DataInput;
+import java.io.DataInputStream;
+import java.io.InputStream;
+import java.net.Socket;
+
+import com.algaworks.chat.cliente.gui.JanelaGui;
+
+public class RecebeMensagemServidor implements Runnable {
+	private Socket socket;
+	private JanelaGui janela;
+
+	public RecebeMensagemServidor(Socket socket, JanelaGui janela) {
+		this.socket = socket;
+		this.janela = janela;
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			try {
+				InputStream is = this.socket.getInputStream();
+				DataInput dis = new DataInputStream(is);
+				String mensagemRecebida = dis.readUTF();
+				janela.adicionaMensagem(mensagemRecebida);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
